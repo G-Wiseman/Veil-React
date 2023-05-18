1e2a14af6f7fcf70e37cfa56c601fdd08ad0fae8
George Wiseman <georgeawiseman@gmail.com>
2023-05-17 21:15:17 -0700

    Minor changes

--- src/PartySelect.js
+++ src/PartySelect.js
@@ -1,16 +1,29 @@
+import "./PartySelect.css"
+
 export default function PartySelect () {
-    var parties = ["Party 1", "Group2"]
+    var parties = ["Golden Genesis", "The Avengers", "The Team"]
+
+    function PartyCard({partyId}) {
+        // Get info on the party
+        
+        return (
+            <div className="PartyCard">
+                <span className="PartyCardName">{partyId}</span>
+                <span>PARTY DESCRIPTION HERE </span>
+                <span>Number of Users: 5</span>
+            </div>
+        )
+    }
 
 
+    return (
+    <div className='party-select'>
+        <div className='parties'>
+            {parties.map((party) => (
+                <PartyCard partyId={party} className='party'></PartyCard>))}
+        </div>
 
-  return (
-  <div className='party-select'>
-      <div className='parties'>
-          {parties.map((party) => (
-              <div className='party'> {party}</div>))}
-      </div>
-
-      SHOW A WAY TO ADD NEW PARTIES?
-  </div>
-  )
+        SHOW A WAY TO ADD NEW PARTIES?
+    </div>
+    )
 }
